148f351ecb3c9dd6426833b851221cdd7c50ec5b
huuvuno1 <nguyenhuuvuno1@gmail.com>
2023-04-03 00:33:09 +0700

    feat: handle class schedue screen

--- src/screens/ClassScheduleScreen/index.js
+++ src/screens/ClassScheduleScreen/index.js
@@ -1,17 +1,48 @@
-import { SCREENS } from "../../constants";
-import { TextInput, Text, ScrollView, View } from "react-native";
+import { Text, ScrollView, View, Image } from "react-native";
 import { useEffect, useState } from "react";
-import { ctmsService } from "../../services";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
+import dateFormat from "dateformat";
 import styles from "./styles";
-const randomColor = () => {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-};
+import { ctmsService } from "../../services";
+import picker from "../../../assets/picker.png";
+import prev from "../../../assets/prev.png";
+import next from "../../../assets/next.png";
+import morning from "../../../assets/morning.png";
+import afternoon from "../../../assets/afternoon.png";
+import evening from "../../../assets/evening.png";
+import teacher from "../../../assets/teacher.png";
+import room from "../../../assets/room.png";
+import status from "../../../assets/status.png";
+import unique from "../../../assets/unique.png";
+import { TouchableOpacity } from "react-native";
 
 const ClassScheduleScreen = ({ navigation }) => {
   const [data, setData] = useState([]);
-  const goToHome = () => {
-    navigation.navigate(SCREENS.HOME);
+  const [startDay, setStartDay] = useState(new Date());
+  const [displayDatePicker, setDisplayDatePicker] = useState(false);
+
+  const handleStartDateChange = (e, date) => {
+    setDisplayDatePicker(false);
+    setStartDay(date);
   };
+
+  const handleShiftWeek = (type) => {
+    let day = new Date(startDay);
+    if (type === "prev") {
+      day.setDate(day.getDate() - 7);
+    } else {
+      day.setDate(day.getDate() + 7);
+    }
+    setStartDay(day);
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await ctmsService.getClassSchedule(startDay, false);
+      setData(data);
+    };
+    getData();
+  }, [startDay]);
 
   useEffect(() => {
     const getData = async () => {
@@ -25,39 +56,97 @@ const ClassScheduleScreen = ({ navigation }) => {
   return (
     <View style={styles.wrapper}>
       <View style={styles.navigate}>
-        <View style={styles.datePicker}></View>
-        <View style={styles.nextWeek}>
-          <Text>{"<<"}</Text>
-        </View>
-        <View style={styles.nextWeek}>
-          <Text>{">>"}</Text>
-        </View>
+        <TouchableOpacity
+          style={styles.datePicker}
+          onPress={() => setDisplayDatePicker(true)}
+        >
+          <Text style={styles.dateLabel}>Tuần từ</Text>
+          <Text style={styles.dateValue}>
+            {dateFormat(startDay, "dd / mm / yyyy")}
+          </Text>
+          {displayDatePicker && (
+            <RNDateTimePicker
+              value={startDay || new Date()}
+              onChange={handleStartDateChange}
+            />
+          )}
+          <Image style={styles.imageItem} source={picker} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.nextWeek}
+          onPress={() => handleShiftWeek("prev")}
+        >
+          <Image style={styles.btnNav} source={prev} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.nextWeek}
+          onPress={() => handleShiftWeek("next")}
+        >
+          <Image style={styles.btnNav} source={next} />
+        </TouchableOpacity>
       </View>
       <ScrollView style={styles.wrapSchedule}>
         {data.map((item, index) => (
-          <View>
+          <View
+            key={index}
+            style={{
+              marginBottom: 10,
+            }}
+          >
             <Text style={styles.scheduleDay}>{item.day}</Text>
-            {item.classes.map((data, index) => (
-              <View style={styles.card}>
+            {item.classes.map((data, index2) => (
+              <View style={styles.card} key={index2}>
                 <View style={styles.leftCard}>
-                  <Text>{data.time}</Text>
-                </View>
-                <View style={styles.rightCard}>
                   <View
                     style={{
                       ...styles.schedule,
-                      backgroundColor: randomColor(),
+                      paddingHorizontal: 15,
+                      paddingTop: 15,
                     }}
                   >
                     <Text style={styles.subjectName}>{data.name}</Text>
-                    <Text>Phòng: {data.room}</Text>
                     <View style={styles.teacher}>
-                      <Text>{"*"}</Text>
-                      <Text>{data.teacher}</Text>
+                      <Image source={room} style={styles.subjectIcon} />
+                      <Text style={styles.bold}>Phòng {data.room}</Text>
                     </View>
-                    <Text>{data.classId}</Text>
-                    <Text>Trạng thái: {data.status}</Text>
+                    <View style={styles.teacher}>
+                      <Image source={teacher} style={styles.subjectIcon} />
+                      <Text style={styles.bold}>{data.teacher}</Text>
+                    </View>
+                    <View style={styles.teacher}>
+                      <Image source={unique} style={styles.subjectIcon} />
+                      <Text style={styles.bold}>{data.classId}</Text>
+                    </View>
+                    <View style={styles.teacher}>
+                      <Image source={status} style={styles.subjectIcon} />
+                      <Text
+                        style={{
+                          ...styles.bold,
+                          color: data.status === "Học" ? "#008000" : "red",
+                        }}
+                      >
+                        {data.status}
+                      </Text>
+                    </View>
                   </View>
+                </View>
+                <View style={styles.rightCard}>
+                  <Text>{data.time.split("->")[0]}</Text>
+                  <Image
+                    source={
+                      data.time.startsWith("07")
+                        ? morning
+                        : data.time.startsWith("13")
+                        ? afternoon
+                        : evening
+                    }
+                    style={{
+                      width: 30,
+                      height: 30,
+                      marginVertical: 7,
+                    }}
+                  />
+                  <Text>{data.time.split("->")[1]}</Text>
                 </View>
               </View>
             ))}
--- src/screens/DashboardScreen/index.js
+++ src/screens/DashboardScreen/index.js
@@ -1,54 +1,60 @@
 import { useEffect, useState } from "react";
-import { Button, Image, ImageBackground, ScrollView, Text, Touchable, TouchableOpacity, View } from "react-native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import * as SecureStore from 'expo-secure-store';
+import {
+  Image,
+  ImageBackground,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as SecureStore from "expo-secure-store";
 import { SCREENS, SECURE_STORE } from "../../constants";
 import styles from "./styles";
-import BlueBg from '../../../assets/blue.jpg'
-import QC from '../../../assets/qc.jpg'
-import schedule from '../../../assets/schedule.png'
-import exam from '../../../assets/exam.png'
-import credit from '../../../assets/credit.png'
-import score from '../../../assets/score.png'
-import bill from '../../../assets/bill.png'
-import post from '../../../assets/post.png'
-import reload from '../../../assets/reload.png'
+import BlueBg from "../../../assets/blue.jpg";
+import QC from "../../../assets/qc.jpg";
+import schedule from "../../../assets/schedule.png";
+import exam from "../../../assets/exam.png";
+import credit from "../../../assets/credit.png";
+import score from "../../../assets/score.png";
+import bill from "../../../assets/bill.png";
+import post from "../../../assets/post.png";
+import reload from "../../../assets/reload.png";
 import { ctmsService } from "../../services";
 import { KEY, repository } from "../../repository";
 
 const Tab = createBottomTabNavigator();
 
 const DashboardScreen = ({ navigation }) => {
-  const [name, setName] = useState()
-  const [subject, setSubject] = useState()
+  const [name, setName] = useState();
+  const [subject, setSubject] = useState();
 
   useEffect(() => {
     (async () => {
-      const value = await SecureStore.getItemAsync(SECURE_STORE.LOGIN_INFO)
+      const value = await SecureStore.getItemAsync(SECURE_STORE.LOGIN_INFO);
       if (!value) {
-        navigation.replace(SCREENS.LOGIN)
+        navigation.replace(SCREENS.LOGIN);
       }
 
-      let user = await repository.getData(KEY.USER_INFO)
-      console.log("data", user)
-      if (!user) {
-        user = await ctmsService.getInfo()
+      let user = await repository.getData(KEY.USER_INFO);
+      console.log("data", user);
+      if (!user || !user.name) {
+        user = await ctmsService.getInfo();
       }
-      setName(user.name)
+      setName(user.name);
 
-      let subjects = await repository.getData(KEY.CLASS_SCHEDULE)?.[0]
+      let subjects = await repository.getData(SCREENS.CLASS_SCHEDULE);
       if (!subjects) {
-        subjects = await ctmsService.getClassSchedule()
+        subjects = await ctmsService.getClassSchedule();
       }
 
-      setSubject(subjects[0])
-
-    })()
-  }, [])
+      setSubject(subjects[0]);
+    })();
+  }, []);
 
   const goToScreen = (screen) => {
-    navigation.navigate(screen)
-  }
+    navigation.navigate(screen);
+  };
 
   return (
     <ScrollView style={styles.wrapper}>
@@ -62,19 +68,28 @@ const DashboardScreen = ({ navigation }) => {
         </View>
       </View>
       <View style={styles.scheduleWrapper}>
-        <ImageBackground source={QC} style={styles.imgBg} >
+        <ImageBackground source={QC} style={styles.imgBg}>
           <View style={styles.wrapperQc}>
             <Text style={styles.qcTitle}>Đã ra mắt:</Text>
             <Text style={styles.qcName}>FITHOU BOT</Text>
-            <Text style={styles.qcName}>Sử dụng ngay trên nền tảng Messenger</Text>
+            <Text
+              style={[
+                styles.qcName,
+                {
+                  fontWeight: "normal",
+                },
+              ]}
+            >
+              Sử dụng ngay trên nền tảng Messenger
+            </Text>
           </View>
           <View style={styles.wrapperBtnQc}>
-            <Text style={styles.btnQc}>Quan tâm</Text>
+            <Text style={styles.btnQc}>Chiến luôn</Text>
           </View>
         </ImageBackground>
       </View>
       <View style={styles.scheduleWrapper}>
-        <ImageBackground source={BlueBg} style={styles.imgBg} >
+        <ImageBackground source={BlueBg} style={styles.imgBg}>
           <View style={styles.wrapperQc}>
             <Text style={styles.qcTitle}>Lịch học gần nhất của bạn</Text>
             <Text style={styles.subject}>Nhập môn công nghệ phần mềm</Text>
@@ -89,32 +104,49 @@ const DashboardScreen = ({ navigation }) => {
       <View>
         <Text style={styles.titleValue}>Dịch vụ CTMS</Text>
         <View style={styles.row}>
-          <TouchableOpacity style={styles.wrapperItem} onPress={() => goToScreen(SCREENS.CLASS_SCHEDULE)}>
+          <TouchableOpacity
+            style={styles.wrapperItem}
+            onPress={() => goToScreen(SCREENS.CLASS_SCHEDULE)}
+          >
             <Image style={styles.imageItem} source={schedule} />
             <Text style={styles.textItem}>Lịch học</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.wrapperItem} onPress={() => goToScreen(SCREENS.EXAM_SCHEDULE)}>
+          <TouchableOpacity
+            style={styles.wrapperItem}
+            onPress={() => goToScreen(SCREENS.EXAM_SCHEDULE)}
+          >
             <Image style={styles.imageItem} source={exam} />
             <Text style={styles.textItem}>Lịch thi</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.wrapperItem} onPress={() => goToScreen(SCREENS.CREDIT)}>
+          <TouchableOpacity
+            style={styles.wrapperItem}
+            onPress={() => goToScreen(SCREENS.CREDIT)}
+          >
             <Image style={styles.imageItem} source={credit} />
             <Text style={styles.textItem}>Tín chỉ</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.wrapperItem} onPress={() => goToScreen(SCREENS.SCORE)}>
+          <TouchableOpacity
+            style={styles.wrapperItem}
+            onPress={() => goToScreen(SCREENS.SCORE)}
+          >
             <Image style={styles.imageItem} source={score} />
             <Text style={styles.textItem}>Điểm số</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.wrapperItem} onPress={() => goToScreen(SCREENS.TUITION_BILL)}>
+          <TouchableOpacity
+            style={styles.wrapperItem}
+            onPress={() => goToScreen(SCREENS.TUITION_BILL)}
+          >
             <Image style={styles.imageItem} source={bill} />
             <Text style={styles.textItem}>Học phí</Text>
           </TouchableOpacity>
         </View>
       </View>
 
-      <View style={{
-        paddingBottom: 100
-      }}>
+      <View
+        style={{
+          paddingBottom: 100,
+        }}
+      >
         <Text style={styles.titleValue2}>Dịch vụ FITHOU</Text>
         <View style={styles.row}>
           <View style={styles.wrapperItem}>
